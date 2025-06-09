7e0d326b6af979a8a4f4fa790737e7274b5c4b9a
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2025-06-09 09:20:17 +0100

    Write quad buffer to memory.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,36 @@
 import Engine from './engine/engine.js';
 import RenderPipelineBuilder from './engine/renderPipeline.js';
+import quadShaderCode from './shaders/quad.wgsl?raw';
 
 export default async function webgpu() {
   const canvas = document.querySelector('canvas');
   const engine  = await Engine.initialize(canvas);
   const device = engine.device;
-  
-  
+
+  const vertexData = new Float32Array([
+    // x,    y
+    -1.0, -1.0, // bottom left
+     1.0, -1.0, // bottom right
+    -1.0,  1.0, // top left
+    -1.0,  1.0, // top left
+     1.0, -1.0, // bottom right
+     1.0,  1.0  // top right
+  ]);
+
+  const vertexBuffer = device.createBuffer({
+    size: vertexData.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+  });
+
+  device.queue.writeBuffer(vertexBuffer, 0, vertexData);
+  const bufferLayout = {
+    arrayStride: 2 * 4,
+    attributes: [
+      { shaderLocation:0 , offset:0, format: 'float32x2'}
+    ]
+  };
   const shaderModule = device.createShaderModule({
-    code: triangleShaderCode
+    code: quadShaderCode
   })
 
   const pipelineBuilder = new RenderPipelineBuilder(device);
@@ -19,7 +41,7 @@ export default async function webgpu() {
     .setTargetFormats([engine.canvasFormat])
     .setPrimitive("triangle-list")
     .build()
-  const commandBuffer = engine.encodeRenderPass(3, renderPipeline, vertexBuffer);
+  const commandBuffer = engine.encodeRenderPass(6, renderPipeline, vertexBuffer);
   await engine.submitCommand(commandBuffer);
 }
 
